e9adf4bd63adaaae52d8823d86b082ae7c0c1986
Sean <seanlengws@gmail.com>
2022-07-21 20:11:07 +0800

    Added landing page design

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,58 +1,62 @@
-import {useAnchorWallet } from '@solana/wallet-adapter-react';
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import React, { FC, useState, useEffect } from 'react';
+import { useAnchorWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import React, { FC, useState, useEffect } from "react";
 
-import styles from '../styles/Home.module.css';
-import SignUp from './SignUp';
-import Feed from './Feed';
-import { getProgram, getStateAccount } from './util';
+import Feed from "./Feed";
+import SignUp from "./SignUp";
+import styles from "../styles/Home.module.css";
+import { getProgram, getStateAccount } from "./util";
 
 const Main: FC = () => {
-	const anchorWallet = useAnchorWallet();
-	const [hasAccount, setHasAccount] = useState(false);
-	const [connected, setConnected] = useState(false);
+    const anchorWallet = useAnchorWallet();
+    const [hasAccount, setHasAccount] = useState(false);
+    const [connected, setConnected] = useState(false);
 
-	useEffect(() => {
-    if (anchorWallet) {
-			showLoginPage()
-			setConnected(true);
-    } else {
-			setConnected(false);
+    useEffect(() => {
+        if (anchorWallet) {
+            showLoginPage();
+            setConnected(true);
+        } else {
+            setConnected(false);
+        }
+    }, [anchorWallet]);
+
+    async function showLoginPage() {
+        if (!anchorWallet) {
+            throw "Something went wrong, wallet is null";
+        }
+        const program = getProgram(anchorWallet);
+        try {
+            await getStateAccount(anchorWallet.publicKey, program);
+            // since we didn't throw an error, we must have an account
+            setHasAccount(true);
+            return;
+        } catch (err) {
+            console.log(err);
+        }
+        // since we errored out, we don't have an account
+        setHasAccount(false);
     }
-  }, [anchorWallet]);
-	
-	async function showLoginPage() {
-		if (!anchorWallet) {
-      throw("Something went wrong, wallet is null");
+
+    async function submitSuccessful() {
+        setHasAccount(true);
     }
-		const program = getProgram(anchorWallet);
-		try {
-			await getStateAccount(anchorWallet.publicKey, program);
-			// since we didn't throw an error, we must have an account
-			setHasAccount(true);
-			return;
-		} catch (err) {
-			console.log(err);
-		}
-		// since we errored out, we don't have an account
-		setHasAccount(false);
-	}
 
-	async function submitSuccessful() {
-		setHasAccount(true);
-	}
+    return (
+        <div className={styles.twitterContainer}>
+            <div className={`${styles.logoContainer}`}>
+                <img src="buildspace.png" className={styles.buildspace_logo} />
+            </div>
 
- 	return (
-		<div className={styles.twitterContainer}> 
-			<h1>Latest Tweets</h1>
-			<WalletMultiButton />
-			<div>
-				{connected ?
-				 	hasAccount ? <Feed /> : <SignUp onSubmit={submitSuccessful}/> 
-					: "please connect your wallet"}
-			</div>
-		</div>
-  	)
-}
+            <div className={styles.registerContainer}>
+                <h1 className={styles.site_header}>Join Bwitter today!</h1>
+                <WalletMultiButton className="button-glow" />
+                <div className={styles.connect_text}>
+                    {connected ? hasAccount ? <Feed /> : <SignUp onSubmit={submitSuccessful} /> : "Please connect your wallet"}
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Main
+export default Main;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,50 +1,53 @@
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import {
-	GlowWalletAdapter,
-	PhantomWalletAdapter,
-	SlopeWalletAdapter,
-	SolflareWalletAdapter,
-	TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
-import { AppProps } from 'next/app';
-import { FC, useMemo } from 'react';
+    GlowWalletAdapter,
+    PhantomWalletAdapter,
+    SlopeWalletAdapter,
+    SolflareWalletAdapter,
+    TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { AppProps } from "next/app";
+import { FC, useMemo } from "react";
 
 // Use require instead of import since order matters
-require('@solana/wallet-adapter-react-ui/styles.css');
-require('../styles/globals.css');
-require('../styles/Home.module.css')
+require("@solana/wallet-adapter-react-ui/styles.css");
+require("../styles/globals.css");
+require("../styles/Home.module.css");
 // require('./Main.css');
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-	// Can be set to 'devnet', 'testnet', or 'mainnet-beta'
-	// const network = WalletAdapterNetwork.Devnet;
-	const network = "http://127.0.0.1:8899";
+    // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+    // const network = WalletAdapterNetwork.Devnet;
+    const network = "http://127.0.0.1:8899";
 
-	// You can also provide a custom RPC endpoint
-	const endpoint = useMemo(() => network, [network]);
+    // You can also provide a custom RPC endpoint
+    const endpoint = useMemo(() => network, [network]);
 
-	// @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-	// Only the wallets you configure here will be compiled into your application, and only the dependencies
-	// of wallets that your users connect to will be loaded
-	const wallets = useMemo(
-		() => [
-			new PhantomWalletAdapter(),
-		],
-		[]
-	);
+    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+    // Only the wallets you configure here will be compiled into your application, and only the dependencies
+    // of wallets that your users connect to will be loaded
+    const wallets = useMemo(
+        () => [
+            new GlowWalletAdapter(),
+            new TorusWalletAdapter(),
+            new SlopeWalletAdapter(),
+            new PhantomWalletAdapter(),
+            new SolflareWalletAdapter(),
+        ],
+        []
+    );
 
-	return (
-		<ConnectionProvider endpoint={endpoint}>
-			<WalletProvider wallets={wallets} autoConnect>
-				<WalletModalProvider>
-					<Component {...pageProps} />
-				</WalletModalProvider>
-			</WalletProvider>
-		</ConnectionProvider>
-	);
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    <Component {...pageProps} />
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
 };
 
 export default App;
